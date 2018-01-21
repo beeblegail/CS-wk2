d14e7a669cdaa6b59d97c6ae399df89366206c22
beeblegail <abbey@pates.me.uk>
2018-01-21 21:36:09 +0000

    added comments
    I have added comments and edited the prep question

--- super extension.py
+++ super extension.py
@@ -3,29 +3,36 @@
 # Title: Week 2 Prep - Super Extension
 # Version 1.0X
 
+# this is checking if the input is a number 
 def askUser(question):
    while True:
       try:
          answerRaw = raw_input(question)
          answer = int(answerRaw)
       except ValueError:
+
+         # this is checking if the input is the letter X
          if answerRaw == 'X' :
             exit() 
          else:
+            # this is informing the user that the number does not work
             print("Not an integer value...")
          continue
       else:
          return (answer)
          break
-        
+
+# this is showing the answers up to a set point        
 def displayResults(start_number, increase, results):
+
    counter = 0
    while counter < results:
       print( counter+1, ': ', start_number+increase*counter )
       counter += 1
    print('Output complete!')
-      
 
+      
+#these lines are to gain input and to show the results to the user
 if __name__ == '__main__':
    while True:
       print "\nAnswer with X to exit:"
