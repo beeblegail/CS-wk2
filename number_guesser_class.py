2094cef2b792081326fd2f480b734caf57409167
beeblegail <35309922+beeblegail@users.noreply.github.com>
2018-01-21 11:27:19 +0000

    number guesser class
    A program where the code selects a random number between 1 and 10 which the person has to guess.

---
+++ number_guesser_class.py
@@ -0,0 +1,8 @@
+
+
+name = (input("Hello, what is your name?") )
+print (" Hello " + str(name) + ", I am thinking of a number between 1 and 10")
+
+
+main = (input(randomnumber() % 10) )
+printf("%d\n", randomnumber);
